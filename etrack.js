39cfa420c823db9d80e21228695baff723e3434d
Omega <wpb911@gmail.com>
2021-01-12 00:33:16 -0500

    updated with views for query interpretation

--- etrack.js
+++ etrack.js
@@ -18,6 +18,9 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   queryAllEmployees();
+  queryAllEmployeesByManager();
+  queryBudgetByDepartment();
+  //queryAllEmployeesByDepartment();
   //queryDanceSongs();
   
   connection.end();
@@ -36,8 +39,37 @@ function queryAllEmployees() {
     console.table(res);
   });
  
-}
 
+}
+function queryAllEmployeesByManager() {
+    const query = connection.query("select * from vepmbyman", function(err, res) {
+      if (err) throw err;
+      // for (var i = 0; i < res.length; i++) {
+      //   console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+      // }
+      // console.log("-----------------------------------");
+      
+      // logs the actual query being run
+      console.log(query.sql);
+      console.table(res);
+    });
+   
+  }
+
+  function queryBudgetByDepartment() {
+    const query = connection.query("select * from vdpart2", function(err, res) {
+      if (err) throw err;
+      // for (var i = 0; i < res.length; i++) {
+      //   console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+      // }
+      // console.log("-----------------------------------");
+      
+      // logs the actual query being run
+      console.log(query.sql);
+      console.table(res);
+    });
+   
+  }
 
 function queryDanceSongs() {
   var query = connection.query("SELECT * FROM songs WHERE genre=?", ["R&B"], function(err, res) {
@@ -53,4 +85,48 @@ function queryDanceSongs() {
 
   // logs the actual query being run
     
+}
+
+function mainSelection() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: 'main',
+            message: "What would you like to do?",
+            choices: ['View All Employees',
+                     'View All Employees By Manager',
+                    'Add Employee', "Remove Employee", 
+                    "Update Employee Role", 
+                    "Update Employee Manager"]
+
+        },
+        {
+            type: "input",
+            name: 'title',
+            message: 'initial price (in dollars)',
+            when: isPosting
+
+        },
+        {
+            type: "input",
+            name: 'title',
+            when: isPosting
+
+        },
+        {
+            type: "input",
+            name: 'title',
+            when: isPosting
+
+        },
+        {
+            type: "input",
+            name: 'title',
+            when: isPosting
+
+        },
+
+
+
+    ])
 }
